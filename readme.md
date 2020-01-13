b7e2750f002a1ff2a9f457e16cb521ded031f639
PauloRSantana <paulorobsantana@gmail.com>
2020-01-13 02:09:36 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+
+<p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
+
+<br>
+
+**Projeto**
+
+Esse projeto utiliza Laravel 5.7 com Mysql.
+
+**Contents**
+
+- [Instalação Laravel](#Instalação)
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+// ROTAS API -------------------------------------------------------------
+
+// Route::get('categories', 'Api\CategoryController@index');
+// Route::post('categories', 'Api\CategoryController@store');
+// Route::put('categories/{id}', 'Api\CategoryController@update');
+// Route::delete('categories/{id}', 'Api\CategoryController@destroy');
+
+
+// Rota API Simplificada (index, store, update, destroy). ----------------
+
+Route::group(['prefix' => 'v1', 'namespace' => 'Api\v1'], function(){
+
+    Route::get('categories/{id}/products', 'CategoryController@products');
+    Route::apiResource('categories', 'CategoryController');
+    
+    Route::apiResource('products', 'ProductController');
+    
+});
